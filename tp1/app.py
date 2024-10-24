b634ebfeb7c4028d02626a36f644c3540b010738
eithan <eithan.nakache@epita.fr>
2024-10-24 14:57:36 +0200

    add model translate

--- tp1/app.py
+++ tp1/app.py
@@ -2,15 +2,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from model_utils import load_model
 import numpy as np
+from transformers import pipeline
 
 app = FastAPI()
 
 model = load_model('regression.joblib')
 
+translator = pipeline('translation_en_to_fr', model='Helsinki-NLP/opus-mt-en-fr')
+
 class PredictionRequest(BaseModel):
     size: float
     nb_rooms: int
     garden: int
+
+class TranslationRequest(BaseModel):
+    text: str
 
 @app.post("/predict")
 def predict(request: PredictionRequest):
@@ -18,3 +24,8 @@ def predict(request: PredictionRequest):
     prediction = model.predict(input_array)
     rounded_prediction = round(prediction[0], 2)
     return {"prediction": rounded_prediction}
+
+@app.post("/translate")
+def translate(request: TranslationRequest):
+    result = translator(request.text)
+    return {"translation": result[0]['translation_text']}
